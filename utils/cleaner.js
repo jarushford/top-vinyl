5a64764e40158e243b4d5c8cb96fd80ff10588de
Jamie Rushford <jar.rushford@gmail.com>
2019-01-28 16:36:35 -0700

    Clean data

--- utils/cleaner.js
+++ utils/cleaner.js
@@ -1,3 +1,44 @@
 const data = require('./albums.json')
+const fs = require('fs')
 
-console.log(data.length)
+const cleanedData = data.reduce((cleanData, album) => {
+  let cleanAlbum = {
+    album: cleanAlbumName(album.album),
+    genre: album.allContent[0],
+    year: album.allContent[data.length - 1],
+    tracks: cleanTracks(album.allTracks),
+    artist: album.artist,
+    rating: `${album.rating} / 5`
+  }
+  cleanData.push(cleanAlbum)
+  return cleanData
+}, [])
+
+function cleanAlbumName(uncleanName) {
+  const nameArray = uncleanName.split('')
+  let foundHyphen = false
+  const cleanName = nameArray.reduce((acc, letter) => {
+    if (letter === '–') {
+      foundHyphen = true
+      return acc
+    } else if (!foundHyphen) {
+      return acc
+    } else {
+      acc += letter
+      return acc
+    }
+  }, '')
+  return cleanName.substring(1)
+}
+
+function cleanTracks(uncleanTracks) {
+  return uncleanTracks.map(track => {
+    if (track.duration === '') {
+      return { duration: 'No Data', track: track.track }
+    } else {
+      return track
+    }
+  })
+}
+
+fs.writeFileSync('utils/cleanData.json', JSON.stringify(cleanedData))
