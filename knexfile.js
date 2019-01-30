9d9065cf7054a9802829ad2dbb12bd72fd16e8c5
Jamie Rushford <jar.rushford@gmail.com>
2019-01-30 11:06:19 -0700

    Configure production env

--- knexfile.js
+++ knexfile.js
@@ -22,6 +22,15 @@ module.exports = {
     seeds: {
       directory: './db/seeds/test'
     }
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
   }
 
 };
